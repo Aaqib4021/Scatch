d2b56c3f086f2c13cd98a4b43a8ec55a4a6da3b9
Aaqib Bashir <aqibnengroo5@gmail.com>
2024-09-14 21:04:06 +0530

    half configuration

--- config/mongoose.connection.js
+++ config/mongoose.connection.js
@@ -1,12 +1,14 @@
 const mongoose = require("mongoose");
+const dbgr = require("debug")("development:mongoose");
+dbgr("Debugging is enabled");
 
 mongoose
   .connect("mongodb://localhost:27017/Scatch")
   .then(function () {
-    console.log(" Db connected");
+    dbgr(" Db connected");
   })
   .catch(function (err) {
-    console.log(err);
+    dbgr(err);
   });
 
 module.exports = mongoose.connection;
